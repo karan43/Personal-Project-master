fa2b3eaa89b40d9b41fd725ef658ca15badba2af
Karan Shrestha <karansth.me@gmail.com>
2021-12-21 12:55:01 +0545

    Features image change

--- src/HomePage/FeatureProduct.js
+++ src/HomePage/FeatureProduct.js
@@ -4,55 +4,92 @@ import ScrollAnimation from 'react-animate-on-scroll';
 
 import './featureproduct.css';
 import '../mediaqueries.css'
-export default class FeatureProduct extends Component{
-  constructor(props){
+export default class FeatureProduct extends Component {
+  constructor(props) {
     super(props);
-    this.state={
-      features:[]
+    this.state = {
+      features: []
     }
   }
-  componentDidMount(){
-    const {featureurl} = this.props;
-      this.setState(
-        {
-          features:featureurl
-        });
+  componentDidMount() {
+    const { featureurl } = this.props;
+    this.setState(
+      {
+        features: featureurl
+      });
   }
-  render(){
-    const{ features }=this.state;
-      if(features.length){
-            return(
-              <div className="features-main">
-              <div className='container'>
-              <h2 className="features-head">FEATURED PRODUCT</h2>
-              {features.map(feature =>{
-                    return(
-                          <div key={feature.id} className="row">
-                              <div className="col-sm-6">
-                                <ScrollAnimation
-                                animateIn='zoomIn'
-                                animateOnce={true}>
-                                  <div className="features-description">
-                                    <h3 className="features-h">{feature.title}</h3>
-                                    <p className="features-p">{feature.description}</p>
-                                  </div>
-                                </ScrollAnimation>
-                              </div>
-                              <div className="col-md-6">
-                                      <div className="laptop-wrapper">
-                                            <iframe width="560" height="315" title='sahek' src="https://www.youtube.com/embed/WEy_deihDfo?rel=0&autoplay=1&loop=1&playlist=WEy_deihDfo”" frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
-                                      </div>
-                              </div>
-                          </div>
-                      );
-              })}
+  render() {
+    const { features } = this.state;
+    if (features.length) {
+      return (
+        <div className="features-main">
+          <div className='container'>
+            <h2 className="features-head">FEATURED PRODUCT</h2>
+            {features.map(feature => {
+              return (
+                <div key={feature.id} className="row">
+                  <div className="col-sm-6">
+                    <ScrollAnimation
+                      animateIn='zoomIn'
+                      animateOnce={true}>
+                      <div className="features-description">
+                        <h3 className="features-h">{feature.title}</h3>
+                        <p className="features-p">{feature.description}</p>
+                      </div>
+                    </ScrollAnimation>
+                  </div>
+                  <div className="col-md-6">
+                    <div className="row">
+                    <div className="col-lg-4 col-md-3 col-sm-3 col-xs-12">
+                      <div className="team-card "  >
+                        <img className="height" src="" alt="" />
+                        <div>
+                          <h3>name</h3>
+                          <p>top</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-lg-4 col-md-3 col-sm-3 col-xs-12">
+                      <div className="team-card" >
+                        <img className="height" src="" alt="" />
+                        <div>
+                          <h3>name</h3>
+                          <p>top</p>
+                        </div>
+                      </div>
+                    </div>
+                    </div>
+                    <div className="row">
+                    <div className="col-lg-4 col-md-3 col-sm-3 col-xs-12">
+                      <div className="team-card" >
+                        <img className="height" src="" alt="" />
+                        <div>
+                          <h3>name</h3>
+                          <p>top</p>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="col-lg-4 col-md-3 col-sm-3 col-xs-12">
+                      <div className="team-card" >
+                        <img className="height" src="" alt="" />
+                        <div>
+                          <h3>name</h3>
+                          <p>top</p>
+                        </div>
+                      </div>
+                    </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
 
-              </div>
-              </div>
-            );
-      }
-      else{
-          return <div className='loading-icon'><Icon name='spinner' size='huge' /></div>
-      }
+          </div>
+        </div>
+      );
+    }
+    else {
+      return <div className='loading-icon'><Icon name='spinner' size='huge' /></div>
+    }
   }
 }
