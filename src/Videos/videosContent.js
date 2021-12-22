85cdd0eeee38b65b5f10384eb5e833a023a7047f
Karan Shrestha <karansth.me@gmail.com>
2021-12-22 14:51:43 +0545

    "Videos content add"

--- src/Videos/videosContent.js
+++ src/Videos/videosContent.js
@@ -3,52 +3,46 @@ import { Card, Icon } from 'semantic-ui-react';
 
 import './videosContent.css';
 
-export default class VidoesContent extends Component{
-  constructor(props){
+export default class VidoesContent extends Component {
+  constructor(props) {
     super(props);
-    this.state={
-      videos:[]
+    this.state = {
+      videos: []
     }
   }
-  
-  componentDidMount(){
-    const {videosurl} = this.props;
-      this.setState(
-        {
-          videos:videosurl
-        });
+
+  componentDidMount() {
+    const { videosurl } = this.props;
+    this.setState(
+      {
+        videos: videosurl
+      });
   }
-  render(){
-    const{ videos }=this.state;
-      if(videos.length){
-          return(
-                  <div className='videos-content-main'>
-                    <div className='container'>
-                     
-                    <div className="row videos-card">
-                      {videos.map(videos =>
-                          <div className='videos-content col-lg-4 col-md-6 col-sm-6 col-xs-12'>
-                            <a href="https://medium.com/" target="_blank" rel="noopener noreferrer" key='id'>
-                              <Card fluid className="videos-ui-card">
-                                <div className="videos-pos-fixed">
-                                  <img src={videos.url} alt='Devops' width="100%" height="70%" />
-                                  <h3>{videos.title}</h3>
-                                  <p>{videos.shortdescription}</p>
-                                  <div className="videos-tag-div">
-                                      <a className="videos-tag" href="/videos"><span>tag</span></a>
-                                  </div>
-                                </div>
-                              </Card>
-                            </a>
-                          </div>
-                      )}
-                    </div>
-                    </div>
-                  </div>
-          );
+  render() {
+    const { videos } = this.state;
+    if (videos.length) {
+      return (
+        <div className="row videos-card">
+          <div className="col-md-4">
+            <div className="laptop-wrapper">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/6CIdvaDDu0g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="laptop-wrapper">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/6CIdvaDDu0g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+          <div className="col-md-4">
+            <div className="laptop-wrapper">
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/6CIdvaDDu0g" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+        </div>
+      );
     }
-    else{
-        return <div className='loading-icon'><Icon name='spinner' size='huge' /></div>
+    else {
+      return <div className='loading-icon'><Icon name='spinner' size='huge' /></div>
     }
   }
 
